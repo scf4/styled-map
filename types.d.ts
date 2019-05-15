600924203e910f0419a55683fe9de4dd451a2477
trpgf29 <michael_martinsky@troweprice.com>
2019-05-15 15:46:07 -0400

    add mapToTheme prop arg to types file

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,5 @@
 declare const styledMap: (...params: any[]) => (props: any) => any;
 
-export const mapToTheme: (key: string) => (props: any) => any;
+export const mapToTheme: (key: string, prop?: string) => (props: any) => any;
 
 export default styledMap;
